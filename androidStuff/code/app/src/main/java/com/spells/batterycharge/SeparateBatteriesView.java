5f3dcd19784fe298d7f3e50288b5f99a5bd7dfa5
AhmedBadee <ahmedbadee17@gmail.com>
2017-12-30 00:20:01 +0200

    added method to draw specified number of batteries as the application needs

--- androidStuff/code/app/src/main/java/com/spells/batterycharge/SeparateBatteriesView.java
+++ androidStuff/code/app/src/main/java/com/spells/batterycharge/SeparateBatteriesView.java
@@ -41,7 +41,6 @@ public class SeparateBatteriesView extends View {
         paintText = new Paint();
         paintText.setColor(0x99212121);
         paintText.setStyle(Paint.Style.FILL);
-        paintText.setTextSize(40);
     }
 
     public SeparateBatteriesView(Context context, int noOfBatteries, float[] readings, float maxReading) {
@@ -65,29 +64,120 @@ public class SeparateBatteriesView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        float width = (float) getWidth();
+        float width  = (float) getWidth();
         float height = (float) getHeight();
 
+        float batteryWidth;
+        float batteryHeight;
+
+        float batteryCapHeight;
+        float batteryTextHeight;
+
+        if (height < 1500) {
+            batteryWidth  = 125;
+            batteryHeight = 250;
+
+            batteryCapHeight   = 20;
+            batteryTextHeight = 30;
+
+            paintText.setTextSize(25);
+        } else {
+            batteryWidth  = 200;
+            batteryHeight = 400;
+
+            batteryCapHeight   = 30;
+            batteryTextHeight = 40;
+
+            paintText.setTextSize(40);
+        }
+
         if (noOfBatteries <= 3) {
-            float lengthBetweenBatteries = ((width - (noOfBatteries * 200)) / (noOfBatteries + 1));
-            float start_x = lengthBetweenBatteries;
-            float start_y = (height / 2) - 200;
+            float start_y = height / 2;
+            drawBatteries(canvas, width, noOfBatteries, start_y, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, readings);
+        } else if (noOfBatteries > 3 && noOfBatteries <= 6) {
+            float start_y = height / 2;
+            if (noOfBatteries == 4) {
+                float topPart = start_y / 2;
+                float[] topBatteriesReadings = {readings[0], readings[1]};
+                drawBatteries(canvas, width, 2, topPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, topBatteriesReadings);
 
-            for (int i = 0; i < noOfBatteries; i++) {
-                float chargePercentage = (readings[i] / maxReading) * 100;
-                float start_x_charge   = start_x;
-                float start_y_charge   = start_y + (400 - (chargePercentage * 4));
+                float[] downBatteriesReadings = {readings[2], readings[3]};
+                float downPart = (float) (start_y * 1.5);
+                drawBatteries(canvas, width, 2, downPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, downBatteriesReadings);
+            } else if (noOfBatteries == 5) {
+                float topPart = start_y / 2;
+                float[] topBatteriesReadings = {readings[0], readings[1]};
+                drawBatteries(canvas, width, 2, topPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, topBatteriesReadings);
 
-                float start_x_cap = start_x + 50;
-                float start_y_cap = start_y - 30;
+                float[] downBatteriesReadings = {readings[2], readings[3], readings[4]};
+                float downPart = (float) (start_y * 1.5);
+                drawBatteries(canvas, width, 3, downPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, downBatteriesReadings);
+            } else if (noOfBatteries == 6) {
+                float topPart = start_y / 2;
+                float[] topBatteriesReadings = {readings[0], readings[1], readings[2]};
+                drawBatteries(canvas, width, 3, topPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, topBatteriesReadings);
 
-                canvas.drawRoundRect(start_x, start_y, start_x + 200, start_y + 400, 4, 4, paintBattery);
-                canvas.drawRoundRect(start_x_charge, start_y_charge, start_x + 200, start_y + 400, 4, 4, paintCharge);
-                canvas.drawRoundRect(start_x_cap, start_y_cap, start_x_cap + 100, start_y_cap + 30, 4, 4, paintCap);
-                canvas.drawText(decimalFormat.format(chargePercentage) + "%", start_x + 50, start_y + 200, paintText);
-                canvas.drawText(decimalFormat.format(readings[i]) + "/" + decimalFormat.format(maxReading), start_x + 30, start_y + 440, paintText);
-                start_x += (lengthBetweenBatteries + 200);
+                float[] downBatteriesReadings = {readings[3], readings[4], readings[5]};
+                float downPart = (float) (start_y * 1.5);
+                drawBatteries(canvas, width, 3, downPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, downBatteriesReadings);
             }
+        } else if (noOfBatteries > 6 && noOfBatteries <= 8) {
+            float start_y = height / 3;
+            if (noOfBatteries == 7) {
+                float firstPart = start_y / 2;
+                float[] firstBatteriesReadings = {readings[0], readings[1]};
+                drawBatteries(canvas, width, 2, firstPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, firstBatteriesReadings);
+
+                float secondPart = (float) (start_y * 1.5);
+                float[] secondBatteriesReadings = {readings[2], readings[3]};
+                drawBatteries(canvas, width, 2, secondPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, secondBatteriesReadings);
+
+                float thirdPart = (float) (start_y * 2.5);
+                float[] thirdBatteriesReadings = {readings[4], readings[5], readings[6]};
+                drawBatteries(canvas, width, 3, thirdPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, thirdBatteriesReadings);
+            } else if (noOfBatteries == 8) {
+                float firstPart = start_y / 2;
+                float[] firstBatteriesReadings = {readings[0], readings[1], readings[2]};
+                drawBatteries(canvas, width, 3, firstPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, firstBatteriesReadings);
+
+                float secondPart = (float) (start_y * 1.5);
+                float[] secondBatteriesReadings = {readings[3], readings[4]};
+                drawBatteries(canvas, width, 2, secondPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, secondBatteriesReadings);
+
+                float thirdPart = (float) (start_y * 2.5);
+                float[] thirdBatteriesReadings = {readings[5], readings[6], readings[7]};
+                drawBatteries(canvas, width, 3, thirdPart, batteryWidth, batteryHeight, batteryCapHeight, batteryTextHeight, thirdBatteriesReadings);
+            }
+        }
+    }
+
+    private void drawBatteries(Canvas canvas,
+                               float width,
+                               int noOfBatteriesToDraw,
+                               float start_y,
+                               float batteryWidth,
+                               float batteryHeight,
+                               float batteryCapHeight,
+                               float batteryTextHeight,
+                               float[] currentBatteriesReadings) {
+        float lengthBetweenBatteries = ((width - (noOfBatteriesToDraw * batteryWidth)) / (noOfBatteriesToDraw + 1));
+        float start_x = lengthBetweenBatteries;
+        start_y -= (batteryHeight / 2);
+
+        for (int i = 0; i < noOfBatteriesToDraw; i++) {
+            float chargePercentage = (currentBatteriesReadings[i] / maxReading) * 100;
+            float start_x_charge   = start_x;
+            float start_y_charge   = start_y + (batteryHeight - (chargePercentage * (batteryHeight / 100)));
+
+            float start_x_cap = start_x + (batteryWidth / 4);
+            float start_y_cap = start_y - batteryCapHeight;
+
+            canvas.drawRoundRect(start_x, start_y, start_x + batteryWidth, start_y + batteryHeight, 4, 4, paintBattery);
+            canvas.drawRoundRect(start_x_charge, start_y_charge, start_x + batteryWidth, start_y + batteryHeight, 4, 4, paintCharge);
+            canvas.drawRoundRect(start_x_cap, start_y_cap, start_x_cap + (batteryWidth / 2), start_y_cap + batteryCapHeight, 4, 4, paintCap);
+            canvas.drawText(decimalFormat.format(chargePercentage) + "%", start_x + (batteryWidth / 4), start_y + (batteryHeight / 2), paintText);
+            canvas.drawText(decimalFormat.format(currentBatteriesReadings[i]) + "/" + decimalFormat.format(maxReading), start_x + (batteryWidth / 6), start_y + (batteryHeight + batteryTextHeight), paintText);
+            start_x += (lengthBetweenBatteries + batteryWidth);
         }
     }
 }
