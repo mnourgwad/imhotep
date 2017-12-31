1d95c6aa160b0727e602be28f0707020ff77b9f5
AhmedBadee <ahmedbadee17@gmail.com>
2018-01-01 00:35:45 +0200

    a demo for connecting the arduino with ESP8266 wifi module to a wifi network to access it form the app

--- androidStuff/arduino_demo/WiFiWebServer/WiFiWebServer.ino
+++ androidStuff/arduino_demo/WiFiWebServer/WiFiWebServer.ino
@@ -1,8 +1,5 @@
 /*
  *  This sketch demonstrates how to set up a simple HTTP-like server.
- *  The server will set a GPIO pin depending on the request
- *    http://server_ip/gpio/0 will set the GPIO2 low,
- *    http://server_ip/gpio/1 will set the GPIO2 high
  *  server_ip is the IP address of the ESP8266 module, will be 
  *  printed to Serial when the module is connected.
  */
